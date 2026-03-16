c7d163f5dcdae001d16053fe66bb4b4258efd71b
Soumya Ranjan Harichandan <skharichandan111@gmail.com>
2026-03-16 21:45:26 +0530

    Managing & Getting User Input via State & Generic Handlers

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
+
 export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [enteredPassword, setEnteredPassword] = useState('');
+
+  const [enteredValues, setEnteredValues] = useState({
+    email: "",
+    password: "",
+  });
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log('Form submitted');
-    // Handle form submission logic here
+    console.log(enteredValues);
   }
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues((prevValue) => ({
+      ...prevValue,
+      [identifier]: value,
+    }));
+  }
+
+  // function handleEmailChange(event) {
+  //   setEnteredEmail(event.target.value);
+  // }
+
+  // function handlePasswordChange(event) {
+  //   setEnteredPassword(event.target.value);
+  // }
+
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
@@ -12,18 +36,30 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event) => handleInputChange("email", event.target.value)}
+            value={enteredValues.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event) => handleInputChange("password", event.target.value)}
+            value={enteredValues.password}
+          />
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button className="button" >Login</button>
+        <button className="button">Login</button>
       </p>
     </form>
   );
