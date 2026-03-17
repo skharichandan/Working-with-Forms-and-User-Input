8e3c3173d3f51089532fbff614d288249bba8d50
Soumya Ranjan Harichandan <skharichandan111@gmail.com>
2026-03-17 15:29:53 +0530

    Outsourcing Validation Logic

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 import Input from "./Input";
+import { isEmail, isNotEmpty, hasMinLength } from "../util/validation.js";
 
 export default function Login() {
-
   const [enteredValues, setEnteredValues] = useState({
     email: "",
     password: "",
@@ -13,11 +13,25 @@ export default function Login() {
     password: false,
   });
 
-  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
-  const passwordIsInvalid = didEdit.password && enteredValues.password.trim().length < 6;
+  const emailIsInvalid =
+    didEdit.email &&
+    !isEmail(enteredValues.email) &&
+    !isNotEmpty(enteredValues.email);
+    
+  const passwordIsInvalid =
+    didEdit.password && !hasMinLength(enteredValues.password, 6);
 
   function handleSubmit(event) {
     event.preventDefault();
+
+    if (emailIsInvalid || passwordIsInvalid) {
+      setDidEdit({
+        email: true,
+        password: true,
+      });
+      return;
+    }
+
 
     console.log(enteredValues);
   }
@@ -40,8 +54,6 @@ export default function Login() {
     }));
   }
 
-  
-
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
@@ -57,17 +69,21 @@ export default function Login() {
           value={enteredValues.email}
           error={emailIsInvalid && "Please enter a valid email address."}
         />
-          <Input 
-            label="Password"
-            id="password"
-            type="password"
-            name="password"
-            onBlur={() => handleInputBlur("password")}
-            onChange={(event) => handleInputChange("password", event.target.value)}
-            value={enteredValues.password}
-            error={passwordIsInvalid && "Password must be at least 6 characters long."}
-          />
-        </div>
+        <Input
+          label="Password"
+          id="password"
+          type="password"
+          name="password"
+          onBlur={() => handleInputBlur("password")}
+          onChange={(event) =>
+            handleInputChange("password", event.target.value)
+          }
+          value={enteredValues.password}
+          error={
+            passwordIsInvalid && "Password must be at least 6 characters long."
+          }
+        />
+      </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
