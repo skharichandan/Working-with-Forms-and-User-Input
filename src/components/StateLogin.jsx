bd2862a24c345b5a0dfb757a32d70a0cf0aab069
Soumya Ranjan Harichandan <skharichandan111@gmail.com>
2026-03-17 14:05:42 +0530

    Validating Input Upon Lost Focus (Blur)

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -6,7 +6,12 @@ export default function Login() {
     password: "",
   });
 
-  const emailIsInvalid = enteredValues.email != "" && !enteredValues.email.includes("@");
+  const [didEdit, setDidEdit] = useState({
+    email: false,
+    password: false,
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -15,19 +20,24 @@ export default function Login() {
   }
 
   function handleInputChange(identifier, value) {
-    setEnteredValues((prevValue) => ({
-      ...prevValue,
+    setEnteredValues((prevValues) => ({
+      ...prevValues,
       [identifier]: value,
+    }));
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false,
     }));
   }
 
-  // function handleEmailChange(event) {
-  //   setEnteredEmail(event.target.value);
-  // }
+  function handleInputBlur(identifier) {
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: true,
+    }));
+  }
 
-  // function handlePasswordChange(event) {
-  //   setEnteredPassword(event.target.value);
-  // }
+  
 
   return (
     <form onSubmit={handleSubmit}>
@@ -40,6 +50,7 @@ export default function Login() {
             id="email"
             type="email"
             name="email"
+            onBlur={() => handleInputBlur("email")}
             onChange={(event) => handleInputChange("email", event.target.value)}
             value={enteredValues.email}
           />
